71fc4aff75c8e8a9a93f672b9cd509649b332f8c
Deadfacer <ewf@tristanshout.com>
2015-04-19 12:51:40 -0400

    bulit models

--- db/migrate/20150419155332_questions.rb
+++ db/migrate/20150419155332_questions.rb
@@ -1,7 +1,10 @@
 class Questions < ActiveRecord::Migration
   def up
   	create_table :questions do |t|
-  		t.string :user
+  		t.string :question_text
+  		t.string :choice_a
+  		t.string :choice_b
+  		t.integer :user_id
   	end
   end
 
